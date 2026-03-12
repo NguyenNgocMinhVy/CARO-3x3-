9d64fbad21dfb636132563cf84ac924036f586fb
NguyenNgocMinhVy <you@example.com>
2026-03-12 23:00:36 +0700

    add caro game source code

---
+++ Caro3x3/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caro3x3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        bool playerTurn = true;
+        Random rand = new Random();
+        Button[] board;
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btThoatGame_Click(object sender, EventArgs e)
+        {
+            Form2 exit = new Form2();
+            exit.Show();
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            board = new Button[]
+            {
+                button1,button2,button3,
+                button4,button5,button6,
+                button7,button8,button9,
+            };
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn.Text == "" && playerTurn) 
+            {
+                btn.Text = "X"; 
+                btn.Enabled = false; 
+
+                // Kiểm tra xem người chơi có thắng không sau nước đi
+                if (CheckWin("X"))
+                {
+                    MessageBox.Show("Bạn đã thắng!");
+                    DisableAllButtons(); 
+                    return; 
+                }
+
+                // Nếu chưa có người thắng, đến lượt máy tính
+                playerTurn = false; 
+                ComputerMove();
+            }
+        }
+
+        private void ComputerMove()
+        {
+         
+            List<int> availableCells = GetAvailableCells();
+
+            int bestMove = FindWinningMove("O"); 
+            if (bestMove != -1)
+            {
+                board[bestMove].Text = "O";
+                board[bestMove].Enabled = false;
+                playerTurn = true; 
+                if (CheckWin("O"))
+                {
+                    MessageBox.Show("Máy tính đã thắng!");
+                    DisableAllButtons();
+                }
+                return;
+            }
+
+            bestMove = FindWinningMove("X"); 
+            if (bestMove != -1)
+            {
+                board[bestMove].Text = "O";
+                board[bestMove].Enabled = false;
+                playerTurn = true;
+                if (CheckWin("O")) 
+                {
+                    MessageBox.Show("Máy tính đã thắng!");
+                    DisableAllButtons();
+                }
+                return;
+            }
+
+            int centerIndex = 4; 
+            if (availableCells.Contains(centerIndex) && board[centerIndex].Text == "")
+            {
+                board[centerIndex].Text = "O";
+                board[centerIndex].Enabled = false;
+                playerTurn = true;
+                return;
+            }
+
+            if (availableCells.Count > 0)
+            {
+                int randomIndex = rand.Next(availableCells.Count);
+                int moveIndex = availableCells[randomIndex];
+                board[moveIndex].Text = "O";
+                board[moveIndex].Enabled = false;
+                playerTurn = true;
+                if (CheckWin("O")) 
+                {
+                    MessageBox.Show("Máy tính đã thắng!");
+                    DisableAllButtons();
+                }
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Hòa cờ!");
+                DisableAllButtons();
+            }
+        }
+
+        private List<int> GetAvailableCells()
+        {
+            List<int> cells = new List<int>();
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (board[i].Text == "")
+                {
+                    cells.Add(i);
+                }
+            }
+            return cells;
+        }
+
+        private bool CheckWin(string playerSymbol)
+        {
+            // Kiểm tra hàng ngang
+            if (CheckLine(0, 1, 2, playerSymbol)) return true;
+            if (CheckLine(3, 4, 5, playerSymbol)) return true;
+            if (CheckLine(6, 7, 8, playerSymbol)) return true;
+
+            // Kiểm tra hàng dọc
+            if (CheckLine(0, 3, 6, playerSymbol)) return true;
+            if (CheckLine(1, 4, 7, playerSymbol)) return true;
+            if (CheckLine(2, 5, 8, playerSymbol)) return true;
+
+            // Kiểm tra đường chéo
+            if (CheckLine(0, 4, 8, playerSymbol)) return true;
+            if (CheckLine(2, 4, 6, playerSymbol)) return true;
+
+            return false;
+        }
+
+        private bool CheckLine(int index1, int index2, int index3, string playerSymbol)
+        {
+            return board[index1].Text == playerSymbol &&
+                   board[index2].Text == playerSymbol &&
+                   board[index3].Text == playerSymbol;
+        }
+
+        private int FindWinningMove(string playerSymbol)
+        {
+            
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (board[i].Text == "") 
+                {
+                    
+                    board[i].Text = playerSymbol;
+
+                    if (CheckWin(playerSymbol))
+                    {
+                        board[i].Text = ""; 
+                        return i; 
+                    }
+                    else
+                    {
+                        board[i].Text = "";
+                    }
+                }
+            }
+            return -1; 
+        }
+
+        
+        private void DisableAllButtons()
+        {
+            foreach (var btn in board)
+            {
+                btn.Enabled = false;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            foreach (var btn in board)
+            {
+                btn.Text = "";
+                btn.Enabled = true;
+            }
+            playerTurn = true; 
+        }
+    }
+}
---
+++ Caro3x3/Form2.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Caro3x3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btbatDau = new System.Windows.Forms.Button();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(384, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(182, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(438, 54);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GAME CARO(3X3)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btbatDau
+            // 
+            this.btbatDau.ForeColor = System.Drawing.Color.Black;
+            this.btbatDau.Location = new System.Drawing.Point(309, 215);
+            this.btbatDau.Name = "btbatDau";
+            this.btbatDau.Size = new System.Drawing.Size(156, 43);
+            this.btbatDau.TabIndex = 2;
+            this.btbatDau.Text = "Bắt Đầu";
+            this.btbatDau.UseVisualStyleBackColor = true;
+            this.btbatDau.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btThoat
+            // 
+            this.btThoat.ForeColor = System.Drawing.Color.Black;
+            this.btThoat.Location = new System.Drawing.Point(309, 277);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(156, 43);
+            this.btThoat.TabIndex = 3;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = true;
+            this.btThoat.Click += new System.EventHandler(this.btThoat_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btThoat);
+            this.Controls.Add(this.btbatDau);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.LightCoral;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "CARO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btbatDau;
+        private System.Windows.Forms.Button btThoat;
+    }
+}
